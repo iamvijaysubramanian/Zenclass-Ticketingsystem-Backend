c3acf489c8e8b6c25d3ac41efb3d64abbf7fce46
iamvijaysubramanian <vijaysubbu69@gmail.com>
2023-01-13 17:35:39 +0530

    package install

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-  
-app.get('/',function(req, res) {
-    const ipAddress = req.socket.remoteAddress;
-    res.send(ipAddress);
-});
-  
-app.listen(5050, () => console.log(`Server is listening on port 5050`))
